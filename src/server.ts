c1178dc2f90e4dfd07b90338416772fa71cebc65
joao victor <joaovprudencio13@gmail.com>
2024-07-09 23:45:58 -0300

    criando a rota e servico de cadastro

--- src/server.ts
+++ src/server.ts
@@ -1,9 +1,18 @@
 import fastify from "fastify";
 import { createTrip } from "./router/cadastro";
-import {z} from 'zod'
+import { z } from 'zod';
+import { serializerCompiler, validatorCompiler } from "fastify-type-provider-zod";
 
 const app = fastify()
+
+
+app.setValidatorCompiler(validatorCompiler);
+app.setSerializerCompiler(serializerCompiler);
+
+
 app.register(createTrip)
+
+
 app.listen({port: 3333}).then(() => {
     
 })
