bc5c8dfee0c52ccc7c2bbd56c1ab4ad266df5b59
joao victor <joaovprudencio13@gmail.com>
2024-07-10 00:33:25 -0300

    simulando seriço de email

--- src/router/cadastro.ts
+++ src/router/cadastro.ts
@@ -1,9 +1,11 @@
-import { doesNotMatch } from "assert";
+
 import { FastifyInstance } from "fastify";
 import { ZodTypeProvider } from "fastify-type-provider-zod";
 import z from "zod";
 import { prisma } from "../lib/prisma";
-
+import dayjs from 'dayjs'
+import nodemailer from 'nodemailer'
+import { getEmail } from "../lib/mail";
 export async function createTrip(app: FastifyInstance){
 
     app.withTypeProvider<ZodTypeProvider>().post("/cadastro", {
@@ -12,17 +14,47 @@ export async function createTrip(app: FastifyInstance){
                 destination: z.string().min(4),
                 starts_at: z.coerce.date(),
                 ends_at: z.coerce.date(),
+                owner_name: z.string(),
+                owner_email: z.string().email(),
             })
         },
     }, async (request) => {
-        const {destination, starts_at, ends_at} = request.body
+        const {destination, starts_at, ends_at, owner_name, owner_email } = request.body
+
+         if(dayjs(starts_at).isBefore(new Date())){
+            throw new Error("data invalida"); 
+         }
+
+      if(dayjs(ends_at).isAfter(starts_at)){
+         throw new Error("data invalida"); 
+      }
+
+
+
          const trip = await prisma.trip.create({
             data: {
                 destination,
                 starts_at,
-                ends_at
+                ends_at,
             }
          })
+        
+         const mail = await getEmail()
+
+        const mensagem = await mail.sendMail({
+            from: {
+                name: "equipe plann",
+                address : "oi@gmail.com"
+            },
+            to: {
+            name : owner_name ,
+            address: owner_email
+            },
+            subject: "testando envio de email",
+            html: `<p>testando envio de email</p>`
+         })
+
+         console.log(nodemailer.getTestMessageUrl(mensagem))
 
         return {tripId: trip.id}
         
